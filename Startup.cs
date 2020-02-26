59d8acd4007b318459ff53e563d9f315b3628f7e
Oleksandr Bodnar <bodnaralex00@gmail.com>
2020-02-26 02:33:00 +0200

    fixed deployment issue

--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,7 @@ namespace Majestics
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "ClientApp/dist/";
             });
 
             services.AddSwaggerGen(c =>
